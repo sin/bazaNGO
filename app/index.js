71837d85b69badb6e16f094c391fdd96cfbb89b6
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-09 00:33:19 +0200

    Fix basename for production

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import styles from './styles.css'
 import configureStore from 'store'
 import createRoutes from 'routes'
 
-const history = createHistory()
+const history = createHistory({ basename: process.env.NODE_ENV === 'production' ? 'bazaNGO' : '/'})
 const store = configureStore(history)
 
 const routes = createRoutes(store, history)
