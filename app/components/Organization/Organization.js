024023e105efe8d6a9de425e79f3922fba4d9d82
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-09 09:45:43 +0200

    Custom fields

--- app/components/Organization/Organization.js
+++ app/components/Organization/Organization.js
@@ -35,14 +35,30 @@ class Home extends Component {
 
   render () {
     const organization = (this.state && this.state.organization) || { empty: true }
-    const { name, tags, purpose, street, street_number, flat_number, postal_code, city, register_at, nip, krs, empty, detail } = organization
+    const { name, tags, purpose, street, street_number, flat_number, postal_code, city, register_at, nip, krs, profile, empty, detail } = organization
+    const hasProfile = profile && (profile.email || profile.facebook || profile.phone_number || profile.www)
+
     return  !empty && !detail ? (
       <div className={container}>
-        <h1 className={nameClass}>{name}</h1>
+        <h1 className={nameClass}>{(profile && profile.name) || name}</h1>
         <div className={orgTags}>
           {tags.map((tag, index) => <span key={index}>{tag}</span>)}
         </div>
         <p>{purpose && purpose.split('. ').map((sentence) => sentence[0].toUpperCase() + sentence.substr(1).toLowerCase()).join('. ')}</p>
+        {hasProfile ?
+          (
+            <span>
+              <h3>Kontakt</h3>
+              <p>
+                {profile.email ? (<span><i className="fa fa-envelope-o" aria-hidden="true" /> <strong>Email:</strong> <a href={profile.email}>{profile.email}</a><br/></span>) : ''}
+                {profile.phone_number ? (<span><i className="fa fa-phone" aria-hidden="true" /> <strong>Telefon:</strong> {profile.phone_number}<br/></span>) : ''}
+                {profile.www ? (<span><i className="fa fa-external-link" aria-hidden="true" /> <strong>WWW:</strong> <a href={profile.www}>{profile.www}</a><br/></span>) : ''}
+                {profile.facebook ? (<span><i className="fa fa-facebook-official" aria-hidden="true" /> <strong>Facebook:</strong> <a href={profile.facebook}>{profile.facebook}</a></span>) : ''}
+              </p>
+            </span>
+          )
+          : ''
+        }
         <h3>Adres</h3>
         <p>
           {street} {street_number}{flat_number ? `/${flat_number}` : ''}<br />
